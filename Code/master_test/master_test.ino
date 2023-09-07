a11bd69e6663cbe21793c292b6150e94f9ce678f
Jason-Chen2 <109184818+Jason-Chen2@users.noreply.github.com>
2023-09-06 21:48:58 -0700

    THT to SMD

--- Code/master_test/master_test.ino
+++ Code/master_test/master_test.ino
@@ -1,29 +1,18 @@
-
-
 #include <Wire.h>
-
 void setup() {
-  Wire.begin(8);        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+Wire.begin(); // join i2c bus
+Serial.begin(9600); // start serial for output
 }
-byte c[4];
+void receiveData() {
+int address = 8;
+int bytesToRead = 6;
+Wire.requestFrom(address, bytesToRead);
+while (Wire.available()) {
+char data = Wire.read();
+Serial.print(data);
+}
+delay(500);
+}
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from peripheral device #8
-  int i = 0;
-  while (Wire.available()) { // peripheral may send less than requested
-    c[i] = Wire.read(); // receive a byte as character
-    i++;
-    Serial.println(i);         // print the character
-  }
-
-  delay(500);
-
-  union Temp {byte ken_b[4]; float ken_f;}Temp_union;
-  Temp_union.ken_b[0] = c[0];
-  Temp_union.ken_b[1] = c[1];
-  Temp_union.ken_b[2] = c[2];
-  Temp_union.ken_b[3] = c[3];
-  float Temp = Temp_union.ken_f;
-  Serial.print("Temperature C:");
-  Serial.println(Temp);
+receiveData();
 }
--- Code/slave_test/slave_test.ino
+++ Code/slave_test/slave_test.ino
@@ -1,54 +1,11 @@
-#include <OneWire.h>
-#include <DallasTemperature.h>
 #include <Wire.h>
-
-
-
-// Data wire is conntec to the Arduino digital pin 4
-#define ONE_WIRE_BUS 5
-
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature sensor 
-DallasTemperature sensors(&oneWire);
-
-void setup(void)
-{
-  // Start serial communication for debugging purposes
-  Serial.begin(9600);
-  // Start up the library
-  sensors.begin();
-   Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+void setup() {
+Wire.begin(8); // join the I2C bus with address 8
+Wire.onReceive(receiveEvent); // call receiveEvent when data is received
 }
-
-void loop(void){ 
-  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  sensors.requestTemperatures(); 
-    Serial.print(sensors, sensors.getAddress(0));
-
-  Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  Serial.print(sensors.getTempCByIndex(0)); 
-  Serial.print(" - Fahrenheit temperature: ");
-  Serial.println(sensors.getTempFByIndex(0));
-  delay(1000);
+void loop() {
+delay(100);
 }
-
-volatile byte* input;
-
-void requestEvent() {
-  byte* data;
-  float ken= sensors.getTempCByIndex(0);
-  //Wire.write((byte)ken); // respond with message of 6 bytes
-  input = (byte*) &ken;
-  data[0] = input[0];
-  data[1] = input[1];
-  data[2] = input[2];
-  data[3] = input[3];
-  Wire.write(data[0]);
-  Wire.write(data[1]);
-  Wire.write(data[2]);
-  Wire.write(data[3]);
+void receiveEvent(int bytes) {
+Wire.write("hello "); // respond with message of 6 bytes as expected by master
 }
