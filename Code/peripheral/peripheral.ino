368e6f10d327a51a96ba8915e00ea76151c1a199
Jason-Chen2 <109184818+Jason-Chen2@users.noreply.github.com>
2023-09-27 00:33:42 -0700

    fix

--- Code/peripheral/peripheral.ino
+++ Code/peripheral/peripheral.ino
@@ -433,7 +433,7 @@ void setup()
     Serial.begin(9600); // start serial for output
     BootScreen();
     digitalWrite(6, HIGH);//enable input 1
-    digitalWrite(7, HIGH);//enable input 2
+    digitalWrite(7, LOW);//enable input 2
   // Set up Dallas Temperature Sensors
   Serial.println("Starting DallasTemperature Library....");
   sensors.begin();                            // start up the dallas temperature library
